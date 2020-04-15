9d46c53031f4f05fe88a142475c93822b72744b8
0xE8551CCB <limeng01@zhihu.com>
2020-04-15 18:47:14 +0800

    f

--- gitc/src/main.rs
+++ gitc/src/main.rs
@@ -1,3 +1,20 @@
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug, Clone)]
+struct Git {
+    path: &'static str,
+}
+
+impl Git {
+    fn new(path: &str) -> Git {
+
+    }
+
+    fn check_existence(&self) -> bool {
+
+    }
 }
