2eea7dc78585ec81ff9b21e8905b5c1e1e0d9ebe
0xE8551CCB <limeng01@zhihu.com>
2020-04-17 18:53:48 +0800

    ✨ feat: replace with structopt

--- tinkv/src/bin/tinkv.rs
+++ tinkv/src/bin/tinkv.rs
@@ -1,56 +1,49 @@
-use clap::{App, Arg, SubCommand, AppSettings};
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::process::exit;
+use structopt::StructOpt;
+
+/// Subcommands
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// Get value from store
+    Get {
+        /// A string key
+        key: String,
+    },
+    /// Save key value to store
+    Set {
+        /// A string key
+        key: String,
+        /// A string value
+        value: String,
+    },
+    #[structopt(name = "rm")]
+    /// Remove value from store
+    Remove {
+        /// A string key
+        key: String,
+    },
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    rename_all = "kebab-case",
+    name = env!("CARGO_PKG_NAME"), 
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+)]
+struct Opt {
+    #[structopt(subcommand)]
+    command: Command,
+}
 
 fn main() {
-    let app = App::new(env!("CARGO_PKG_NAME"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .setting(AppSettings::VersionlessSubcommands)
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("Get value from store")
-                .arg(
-                    Arg::with_name("key")
-                        .required(true)
-                        .help("A string key")
-                        .value_name("KEY"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("Set key value to store")
-                .arg(
-                    Arg::with_name("key")
-                        .help("A string key")
-                        .required(true)
-                        .value_name("KEY"),
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .help("A string value")
-                        .required(true)
-                        .value_name("VALUE"),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("Remove key value from store")
-                .arg(
-                    Arg::with_name("key")
-                        .help("A string key")
-                        .required(true)
-                        .value_name("KEY"),
-                ),
-        );
-
-    let m = app.get_matches();
-    match m.subcommand() {
-        ("get", Some(_)) => unimplemented(),
-        ("set", Some(_)) => unimplemented(),
-        ("rm", Some(_)) => unimplemented(),
-        _ => unreachable!(),
+    let opt = Opt::from_args();
+    match opt.command {
+        Command::Get { key: _key } => unimplemented(),
+        Command::Set { key: _key, value: _value } => unimplemented(),
+        Command::Remove { key: _value} => unimplemented(),
     }
 }
 
